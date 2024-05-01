30f7de1681df17c47c8ecc1de1036ca375ac19a3
Vu Bui <vu@thebu.is>
2024-04-30 23:17:48 -0500

    first draft

--- createmoba.py
+++ createmoba.py
@@ -1,1 +1,12 @@
+input_file_name = "servers.txt"
+with open(input_file_name, "r") as file:
+  server_list = file.read().splitlines()
 
+result_string = ""
+for server in server_list:
+  print(server)
+  result_string += server + ".s02.chevron.net\n"
+
+output_file_name = "moba.txt"
+with open(output_file_name, "w") as file:
+  file.write(result_string)
